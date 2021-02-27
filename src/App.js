96e27652e0edc4e5793f87405bc4840821fe762c
Thien Pham <phamanhthien1907@gmail.com>
2021-02-27 22:45:49 +0700

    Fix syntax

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import './App.css';
 
 function App() {
   return (
+    <>
     <h1>Hello this is new Store !</h1>
     <span>And this line is demonstrate you make CI successed !</span>
+    </>
   );
 }
 
